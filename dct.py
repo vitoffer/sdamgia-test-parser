43aba465f88b26fbcef17ccd09bb2ab29889e76c
vitoffer <ms.titoff1011@yandex.ru>
2024-12-23 19:45:28 +0300

    add full parsed dictionary with tasks and answers

---
+++ dct.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+links_for_search_answers = [
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=79&cat_id[]=90&cat_id[]=96&cat_id[]=102&cat_id[]=94&cat_id[]=111&cat_id[]=112&cat_id[]=113&cat_id[]=114&filter=all',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=182&filter=all',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=192&cat_id[]=193&cat_id[]=180&cat_id[]=148&cat_id[]=140&cat_id[]=178&cat_id[]=177&cat_id[]=197&cat_id[]=194&cat_id[]=144&cat_id[]=151&filter=all',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=166&filter=all',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=185&cat_id[]=265&filter=all',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=14&cat_id[]=9&cat_id[]=10&cat_id[]=11&cat_id[]=12&cat_id[]=13&filter=all',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=55&cat_id[]=60&cat_id[]=57&cat_id[]=62&cat_id[]=56&cat_id[]=61&cat_id[]=58&cat_id[]=63&cat_id[]=65&cat_id[]=59&cat_id[]=64&filter=all',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=69&cat_id[]=68&cat_id[]=70&cat_id[]=183&filter=all',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=71&cat_id[]=72&cat_id[]=76&cat_id[]=77&cat_id[]=73&cat_id[]=74&cat_id[]=75&cat_id[]=184&filter=all',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=88&cat_id[]=84&cat_id[]=85&cat_id[]=86&cat_id[]=87&cat_id[]=89&filter=all',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=267&cat_id[]=294&cat_id[]=125&cat_id[]=122&cat_id[]=272&cat_id[]=191&cat_id[]=296&filter=all',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=175&cat_id[]=81&cat_id[]=83&cat_id[]=82&cat_id[]=80&cat_id[]=78&filter=all',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=79&cat_id[]=90&cat_id[]=96&cat_id[]=102&cat_id[]=94&cat_id[]=111&cat_id[]=112&cat_id[]=113&cat_id[]=114&filter=all_a',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=182&filter=all_a',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=192&cat_id[]=193&cat_id[]=180&cat_id[]=148&cat_id[]=140&cat_id[]=178&cat_id[]=177&cat_id[]=197&cat_id[]=194&cat_id[]=144&cat_id[]=151&filter=all_a',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=166&filter=all_a',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=185&cat_id[]=265&filter=all_a',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=14&cat_id[]=9&cat_id[]=10&cat_id[]=11&cat_id[]=12&cat_id[]=13&filter=all_a',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=55&cat_id[]=60&cat_id[]=57&cat_id[]=62&cat_id[]=56&cat_id[]=61&cat_id[]=58&cat_id[]=63&cat_id[]=65&cat_id[]=59&cat_id[]=64&filter=all_a',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=69&cat_id[]=68&cat_id[]=70&cat_id[]=183&filter=all_a',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=71&cat_id[]=72&cat_id[]=76&cat_id[]=77&cat_id[]=73&cat_id[]=74&cat_id[]=75&cat_id[]=184&filter=all_a',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=88&cat_id[]=84&cat_id[]=85&cat_id[]=86&cat_id[]=87&cat_id[]=89&filter=all_a',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=267&cat_id[]=294&cat_id[]=125&cat_id[]=122&cat_id[]=272&cat_id[]=191&cat_id[]=296&filter=all_a',
+	'https://ege.sdamgia.ru/test?a=view_many&cat_id[]=175&cat_id[]=81&cat_id[]=83&cat_id[]=82&cat_id[]=80&cat_id[]=78&filter=all_a',
+]
+
+dct = {}
+
+for link in links_for_search_answers:
+	r = requests.get(link)
+	request_text = r.text.replace("&shy", '').replace(";\xad", "").replace("\xad", "")
+	soup_reponses = BeautifulSoup(request_text, 'html.parser')
+
+	maindiv_answers = soup_reponses.find_all(name="div", attrs={"class": "prob_maindiv"})
+
+	for maindiv_answer in maindiv_answers:
+		answer_soup = BeautifulSoup(str(maindiv_answer), 'html.parser')
+		answer_id = answer_soup.find(name="div", attrs={"class": "pbody"})['id']
+		answer_div = answer_soup.find(name="div", attrs={"class": "answer"})
+
+		if not answer_div:
+			print(maindiv_answer)
+
+		ans_text = answer_div.get_text()
+		print(f"{answer_id}: {ans_text[ans_text.rfind(': ') + 2:]}")
+		dct[answer_id] = ans_text[ans_text.rfind(': ') + 2:]
+
+
+with open("dictionary.json", "w+") as f:
+    json.dump(dct, f)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
-import requests
+import json
+
 
 with open('./input.txt', encoding="utf-8") as f:
 	text = " ".join(list(f.readlines()))
@@ -17,45 +18,51 @@ base_url = "https://ege.sdamgia.ru/search"
 
 k = 1
 
+dct = json.load(open('dictionary.json', 'r'))
+
 for task in tasks:
 	task_id = BeautifulSoup(str(task), 'html.parser').find(attrs={"class": "pbody"})['id']
-	usable_task = task.get_text()[:200]
-	mx_ind_znak = max(usable_task.rfind(" "),
-									 usable_task.rfind("."),
-									 usable_task.rfind(","),
-									 usable_task.rfind("?"))
-	usable_task = usable_task[:mx_ind_znak]
-
-	n_page = 1
-	ans = []
-	while len(ans) == 0:
-		params = {"search": usable_task, "cb": 1, "body": 3, 'page': n_page}
-		try:
-			r = requests.get(base_url, params=params, timeout=1)
-			# print(k)
-		except requests.exceptions.Timeout:
-			print(f'{k}) *пропущен')
-			break
-		except requests.exceptions.RequestException as e:
-			print(f"Произошла ошибка: {e}")
-			break
-
-		request_text = r.text.replace("&shy", '').replace(";\xad", "").replace("\xad", "")
-		soup_reponses = BeautifulSoup(request_text, 'html.parser')
-		maindiv_answers = soup_reponses.find_all(name="div", attrs={"class": "prob_maindiv"})
-
-		for maindiv_answer in maindiv_answers:
-			answer_soup = BeautifulSoup(str(maindiv_answer), 'html.parser')
-			answer_id = answer_soup.find(name="div", attrs={"class": "pbody"})['id']
-			answer_div = answer_soup.find(name="div", attrs={"class": "answer"})
-
-			if answer_id == task_id:
-				ans_text = answer_div.get_text()
-				ans.append(ans_text[ans_text.rfind(': ') + 2:])
-
-		n_page += 1
-
-	if len(ans):
-		print(str(k) + ')', " или ".join(ans))
-
+	print(str(k) + ')', dct[task_id])
 	k += 1
+
+
+	# usable_task = task.get_text()[:200]
+	# mx_ind_znak = max(usable_task.rfind(" "),
+	# 								 usable_task.rfind("."),
+	# 								 usable_task.rfind(","),
+	# 								 usable_task.rfind("?"))
+	# usable_task = usable_task[:mx_ind_znak]
+
+	# n_page = 1
+	# ans = []
+	# while len(ans) == 0:
+	# 	params = {"search": usable_task, "cb": 1, "body": 3, 'page': n_page}
+	# 	try:
+	# 		r = requests.get(base_url, params=params, timeout=1)
+	# 		# print(k)
+	# 	except requests.exceptions.Timeout:
+	# 		print(f'{k}) *пропущен')
+	# 		break
+	# 	except requests.exceptions.RequestException as e:
+	# 		print(f"Произошла ошибка: {e}")
+	# 		break
+
+	# 	request_text = r.text.replace("&shy", '').replace(";\xad", "").replace("\xad", "")
+	# 	soup_reponses = BeautifulSoup(request_text, 'html.parser')
+	# 	maindiv_answers = soup_reponses.find_all(name="div", attrs={"class": "prob_maindiv"})
+
+	# 	for maindiv_answer in maindiv_answers:
+	# 		answer_soup = BeautifulSoup(str(maindiv_answer), 'html.parser')
+	# 		answer_id = answer_soup.find(name="div", attrs={"class": "pbody"})['id']
+	# 		answer_div = answer_soup.find(name="div", attrs={"class": "answer"})
+
+	# 		if answer_id == task_id:
+	# 			ans_text = answer_div.get_text()
+	# 			ans.append(ans_text[ans_text.rfind(': ') + 2:])
+
+	# 	n_page += 1
+
+	# if len(ans):
+	# 	print(str(k) + ')', " или ".join(ans))
+
+	# k += 1
