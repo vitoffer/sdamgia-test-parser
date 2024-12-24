1add340475d6f7383daad9b98a5b1de19eced609
vitoffer <ms.titoff1011@yandex.ru>
2024-12-24 19:07:21 +0300

    add rus test solver

---
+++ rus.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import json
+
+
+with open('./input.txt', encoding="utf-8") as f:
+	text = " ".join(list(f.readlines()))
+	text = text.replace("&shy", '').replace(";\xad", "").replace("\xad", "")
+
+soup = BeautifulSoup(text, 'html.parser')
+
+all_divs = soup.find_all(name="div", attrs={"class": "pbody"})
+tasks = []
+for div in all_divs:
+	tasks.append(div)
+print(len(tasks))
+
+k = 1
+
+dct = json.load(open('rus.json', 'r'))
+
+for task in tasks:
+	task_text = BeautifulSoup(str(task), 'html.parser').find(attrs={"class": "pbody"})
+	if not task_text.has_attr('id'):
+		continue
+	task_id = task_text['id']
+	print(str(k) + ')', dct[task_id])
+	k += 1
---
+++ rus_dict.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+with open('./all_tasks.txt', encoding="utf-8") as f:
+	all_tasks = " ".join(list(f.readlines()))
+
+all_tasks = all_tasks.replace("&shy", '').replace(";\xad", "").replace("\xad", "")
+
+soup = BeautifulSoup(all_tasks, 'html.parser')
+
+themes = soup.find_all(name='div', attrs={"class": "Theme nobg"})
+links_for_search_answers = []
+for theme in themes[:26]:
+  links_for_search_answers.append(
+		f"https://rus-ege.sdamgia.ru{theme.findChild(name='a', attrs={'class': 'Theme-link'}, recursive=False)['href']}"
+	)
+
+
+dct = {}
+
+for link in links_for_search_answers:
+	r = requests.get(link)
+	request_text = r.text.replace("&shy", '').replace(";\xad", "").replace("\xad", "")
+	soup_reponses = BeautifulSoup(request_text, 'html.parser')
+
+	maindiv_answers = soup_reponses.find_all(name="div", attrs={"class": "prob_maindiv"})
+
+	for maindiv_answer in maindiv_answers:
+
+		maindiv_answer = maindiv_answer.parent
+		divs = maindiv_answer.find_all(name="div", attrs={"class": "pbody"})
+		for answer in divs:
+			if answer.has_attr('id'):
+				answer_id = answer['id']
+				break
+		answer_div = maindiv_answer.find(name="div", attrs={"class": "answer"})
+
+		if not answer_div:
+			print(maindiv_answer)
+
+		ans_text = answer_div.get_text()
+		# print(f"{answer_id}: {ans_text[ans_text.rfind(': ') + 2:]}")
+		dct[answer_id] = ans_text[ans_text.rfind(': ') + 2:]
+
+	with open("rus.json", "a") as file:
+		file.truncate()
+
+	with open("rus.json", "w+") as f:
+			json.dump(dct, f)
